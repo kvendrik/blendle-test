97d5478411a9208d3a198d227bebbdf5dff7f9d7
Koen Vendrik <k.vendrik@gmail.com>
2016-02-12 23:33:45 +0100

    Makes sharing buttons work

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,9 +1,11 @@
 var dropdowns = require('./modules/dropdowns'),
 	navigationHelper = require('./modules/navigation-helper'),
 	lightbox = require('./modules/lightbox'),
-	tooltips = require('./modules/tooltips');
+	tooltips = require('./modules/tooltips'),
+	share = require('./modules/data-share');
 
 navigationHelper.init();
 dropdowns.init();
 lightbox.init();
 tooltips.init();
+share.init();
---
+++ dev/js/modules/data-share.js
@@ -0,0 +1,41 @@
+module.exports = {
+
+	init: function(){
+		this._bindEvents();
+	},
+
+	_bindEvents: function(){
+		var self = this,
+			triggers = document.querySelectorAll('[data-share]');
+		
+		[].forEach.call(triggers, function(el){
+			el.addEventListener('click', self._handleShareClick, false);
+		});
+	},
+
+	_handleShareClick: function(platform){
+		var platformName = this.getAttribute('data-share'),
+			shareDesc = this.getAttribute('data-share-desc'),
+			shareUrl = this.getAttribute('data-share-url');
+
+		var winH = 350,
+			winW = 520;
+
+		var openUrl = function(url, name){
+			window.open(url, name, 'top='+((window.innerHeight/2) - (winH/2))+',left='+((window.innerWidth/2) - (winW/2))+',toolbar=0,status=0,width='+winW+',height='+winH);
+		};
+
+		switch(platformName){
+			case 'twitter':
+				openUrl('https://twitter.com/intent/tweet?text='+shareDesc+' '+shareUrl, 'Twitter');
+				break;
+			case 'facebook':
+				openUrl('http://www.facebook.com/sharer.php?s=100&p[title]=Blendle&p[summary]='+shareDesc+'&p[url]='+shareUrl, 'Facebook');
+				break;
+			case 'linkedin':
+				openUrl('https://www.linkedin.com/shareArticle?mini=true&url='+shareUrl+'&summary='+shareDesc);
+				break;
+		}
+	}
+	
+};
