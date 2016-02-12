c3723476c00ec3fc9e718355617685d43a2c31df
Koen Vendrik <k.vendrik@gmail.com>
2016-02-12 17:44:15 +0100

    Makes historyState work

--- dev/js/modules/navigation-helper.js
+++ dev/js/modules/navigation-helper.js
@@ -1,4 +1,4 @@
-//1. fixed header
+//1. x fixed header
 //2. history state
 //3. like btn
 //4. img zoom
@@ -8,18 +8,55 @@ module.exports = {
 	init: function(){
 		this._articleViewTransDur = this._getTransitionDurationInMilliSec();
 		this._bindEvents();
+		this._checkHash();
 	},
 
 	_bindEvents: function(){
 		var self = this,
-			cards = document.getElementsByClassName('js-article-card'),
+			triggers = document.querySelectorAll('[data-article-open]'),
 			closeBtn = document.getElementById('js-article-view-close-btn');
 
-		[].forEach.call(cards, function(el){
-			el.addEventListener('click', self._openView.bind(self), false);
+		var onTriggerClick = function(){
+			var slug = this.getAttribute('data-article-open');
+			history.pushState({
+				slug: slug
+			}, null, '/#/article/'+slug);
+			self._openView();
+		};
+
+		var onCloseBtnClick = function(){
+			history.pushState({}, null, '/');
+			self._closeView();
+		};
+
+		[].forEach.call(triggers, function(el){
+			el.addEventListener('click', onTriggerClick, false);
 		});
 
-		closeBtn.addEventListener('click', this._closeView.bind(self), false);
+		closeBtn.addEventListener('click', onCloseBtnClick, false);
+
+		window.addEventListener('popstate', this._handlePopstate.bind(self), false);
+	},
+
+	_checkHash: function(){
+		var hash = location.hash;
+
+		if(hash){
+			var slug = hash.replace('/#/article/', '');
+			this._openView();
+		}
+	},
+
+	_handlePopstate: function(e){
+		var state = e.state;
+
+		if(state && state.slug){
+			//is an article
+			this._openView();
+		} else {
+			//is root
+			this._closeView();
+		}
 	},
 
 	_getTransitionDurationInMilliSec: function(){
@@ -29,7 +66,7 @@ module.exports = {
 		return number*1000;
 	},
 
-	_openView: function(){
+	_openView: function(slug){
 		document.body.classList.add('article-view--block');
 		setTimeout(function(){
 			document.body.classList.add('article-view--visible');
@@ -38,7 +75,6 @@ module.exports = {
 
 	_closeView: function(){
 		document.body.classList.remove('article-view--visible');
-
 		setTimeout(function(){
 			document.body.classList.remove('article-view--block');
 		}, this._articleViewTransDur);
